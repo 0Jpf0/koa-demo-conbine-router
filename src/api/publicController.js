a33dd2e5202c27be72b2206300a6c3dae2979b2f
0Jpf0 <897586577@qq.com>
2019-12-05 16:01:40 +0800

    //

--- src/api/publicController.js
+++ src/api/publicController.js
@@ -2,7 +2,13 @@ import svgCaptcha from 'svg-captcha';
 class publicController{
     constructor(){}
     async getCaptcha(ctx){
-        let captcha=svgCaptcha.create();
+        let captcha=svgCaptcha.create({
+            size: 4,
+            color:true,
+            noise:Math.floor(Math.random()*5),
+            width:150,
+            height:50,
+        });
         ctx.body={
             code:200,
             data:captcha.data

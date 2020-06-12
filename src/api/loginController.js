05cbb677ed15f8b7f62cb2911a490faf82d3f205
pengxuemei <897586577@qq.com>
2020-06-12 17:36:33 +0800

    注册业务逻辑，bcrypt密码处理

--- src/api/loginController.js
+++ src/api/loginController.js
@@ -2,6 +2,8 @@ import jsonwentoken from "jsonwebtoken";
 import config from "../config";
 import { checkCode } from "../common/untils";
 import User from "../model/User";
+import moment from "moment";
+import bcrypt from "bcrypt";
 class loginController {
   constructor() {}
 
@@ -15,7 +17,7 @@ class loginController {
         username: body.username,
       });
       if (user != null) {
-        if (user.password === body.password) {
+        if (await bcrypt.compare(body.password, user.password)) {
           checkUserPassword = true;
         } else {
           checkUserPassword = false;
@@ -47,6 +49,54 @@ class loginController {
         msg: "图形验证码不正确",
       };
     }
+  }
+  async reg(ctx) {
+    let { body } = ctx.request;
+    let sid = body.sid;
+    let code = body.code;
+    let msg = {};
+    if (await checkCode(sid, code)) {
+      let checkResult = true;
+      let user = await User.findOne({
+        username: body.username,
+      });
+      let user1 = await User.findOne({
+        name: body.name,
+      });
+      if (user != null && user != "undefined") {
+        if (user.username === body.username) {
+          msg.username = ["该用户名已注册，可通过电子邮件找回密码"];
+          checkResult = false;
+        }
+      }
+      if (user1 != null && user1 != "undefined") {
+        if (user1.name === body.name) {
+          msg.username = ["该昵称已存在，请重新输入"];
+          checkResult = false;
+        }
+      }
+      if (checkResult) {
+        body.password = await bcrypt.hash(body.password, 5);
+        let newuser = new User({
+          username: body.username,
+          name: body.name,
+          password: body.password,
+          created: moment().format("YYYY-MM-DD HH:mm:ss"),
+        });
+        let result = newuser.save();
+        ctx.body = {
+          code: 200,
+          data: result,
+          msg: "注册成功",
+        };
+      }
+    } else {
+      msg.code = ["图形验证码已失效，请重新输入"];
+    }
+    ctx.body = {
+      code: 500,
+      msg: msg,
+    };
   }
 }
 
--- src/routers/loginRouter.js
+++ src/routers/loginRouter.js
@@ -4,4 +4,5 @@ import loginController from "../api/loginController";
 const router = new koaRouter();
 router.prefix("/login");
 router.post("/login", loginController.login);
+router.post("/reg", loginController.reg);
 export default router;
